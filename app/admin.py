bacaff3e76c17225c7e51031755bf6d6ae14e0bf
Javier Silva <ja.silva@duocuc.cl>
2022-05-06 14:13:21 -0400

    first commit

---
+++ app/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ["nombre", "marca", "precio", "categoria","stock","imagen"]
+    list_editable = ["precio"]
+    search_fields = ["nombre","stock"]
+    list_filter = ["categoria"]
+    list_per_page = 10
+
+class ProductoAdminDestacados(admin.ModelAdmin):
+    list_display = ["nombre", "marca", "precio", "categoria","stock","imagen"]
+    list_editable = ["precio"]
+    search_fields = ["nombre","stock"]
+    list_filter = ["categoria"]
+    list_per_page = 10
+
+
+admin.site.register(Categorias)
+admin.site.register(Productos, ProductoAdmin)
+admin.site.register(ProductosDestacados, ProductoAdminDestacados)
+admin.site.register(Marcas)
+
